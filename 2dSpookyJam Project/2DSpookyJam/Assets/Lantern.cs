8e0a24c229952b10f80e3d1983b3bdc70d965321
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-10-20 18:22:02 -0600

    lantern polish

--- 2dSpookyJam Project/2DSpookyJam/Assets/Lantern.cs
+++ 2dSpookyJam Project/2DSpookyJam/Assets/Lantern.cs
@@ -7,26 +7,35 @@ using UnityEngine.Rendering.Universal;
 
 public class Lantern : MonoBehaviour
 {
-    [SerializeField] private Light2D light;
-
+    [SerializeField] private Light2D light;       
     [SerializeField] private float intensityMax;
+    [SerializeField] private float intensityMin = 0;
+
+    [SerializeField] private float outerMax;
+    [SerializeField] private float outerMin;
+    private float outerRange;
+    
     [SerializeField] private float lanternOilLevelCurrent;
     [SerializeField] private float lanternOilLevelMax;
     [SerializeField] private float lanternOilDecreasePerSecond;
     [Tooltip("how much oil is added to the lantern when refilled")]
     [SerializeField] private float lanternRefillAmount;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
         intensityMax = light.intensity;
         lanternOilLevelCurrent = lanternOilLevelMax;
+        outerRange = outerMax - outerMin;
     }
 
     // Update is called once per frame
     void Update()
     {
         light.intensity = (lanternOilLevelCurrent/ lanternOilLevelMax) * intensityMax;
+        light.pointLightOuterRadius= ((lanternOilLevelCurrent / lanternOilLevelMax) * outerRange ) + outerMin;
         lanternOilLevelCurrent = Mathf.Max( lanternOilLevelCurrent-  lanternOilDecreasePerSecond * Time.deltaTime ,  0); 
         
     }
