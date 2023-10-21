6ba7330f8b67066595e0505cbe654f7ef153b404
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-10-21 14:48:13 -0600

    ghost is theoretically working - no extensive testing, player controller seems to be broken

--- 2dSpookyJam Project/2DSpookyJam/Assets/Scripts/Ghost.cs
+++ 2dSpookyJam Project/2DSpookyJam/Assets/Scripts/Ghost.cs
@@ -4,53 +4,198 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rigidBody;
+    private GameManager gameManager;
+
+    private enum GhostState { idle, curious, hostile, wary} //consider refactoring idle to patrol
+    [SerializeField] private GhostState ghostState;
+    
+
+    //[SerializeField] private Rigidbody2D rigidBody;
 
     [SerializeField] private Vector2 target;
+    [Tooltip("theoretically set by referencing GameManager.playerController")]
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject lamp;
+    private Transform lastSeenPlayerPos;
 
-    private const float SLOW_SPEED = 1;
-    private const float FAST_SPEED = 0.5f;
+    [SerializeField] private float innerRange;
+    [SerializeField] private float outerRange;
+
+    [SerializeField] private float fastSpeed = 1;
+    [SerializeField] private float slowSpeed = 0.5f;
+    private float currentSpeed ;
 
     private int state;
-    private bool lampLit = false;
+    [SerializeField] private bool lampLit = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        gameManager = GameManager.Instance;
+        //rigidBody = GetComponent<Rigidbody2D>();
+
+        currentSpeed = slowSpeed;
+
+        player = gameManager.playerController.gameObject;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void FixedUpdate()
     {
+        checkPlayerPos();
+        if(ghostState == GhostState.idle)
+        {
+            //call patrol();
+        }
+        Vector2 moveTo = Vector2.MoveTowards(transform.position, target, currentSpeed);
+        if(moveTo.magnitude >= .01)
+        {
+            this.transform.position = moveTo;
+        }
+    }
+    
+
+    //void FixedUpdate()
+    //{
+        /*
+        rigidBody.position += Vector2.MoveTowards(rigidBody.position, target, currentSpeed);
+
+
+
         if (lampLit) return;
         switch (state)
         {
-            case 0:
+            case 0: //idle
                 rigidBody.position += Vector2.MoveTowards(rigidBody.position, target, SLOW_SPEED);
                 break;
-            case 1:
+            case 1: //curious
                 rigidBody.position += Vector2.MoveTowards(rigidBody.position, target, SLOW_SPEED);
                 break;
-            case 2:
+            case 2: // hostile?
                 rigidBody.position += Vector2.MoveTowards(rigidBody.position, target, FAST_SPEED);
                 break;
-            case 3:
+            case 3:  // wary?
                 rigidBody.position += Vector2.MoveTowards(rigidBody.position, target, FAST_SPEED);
                 break;
+        }*/
+
+    //}
+
+
+    private void checkPlayerPos()
+    {
+        float distanceToPlayer = Vector2.Distance(player.transform.position, this.transform.position);
+        switch (ghostState)
+        {
+            case GhostState.idle: //if we're in idle state
+                if (distanceToPlayer <= outerRange) //player goes from being idle to entering outer circle
+                {
+                    ghostState = GhostState.curious;
+                    target = player.transform.position;
+                    currentSpeed = slowSpeed;
+
+                    break;
+                }
+                else if (distanceToPlayer <= innerRange)
+                { //and we enter the inner circle
+                    ghostState = GhostState.hostile;
+                    currentSpeed = fastSpeed;
+
+                    target = player.transform.position;
+                    break;
+                }
+
+                break;
+            case GhostState.curious: //if we're in curious state (state 1)
+                if (distanceToPlayer >= outerRange) //player exits outer range 
+                {
+                    Debug.Log("this should only happen once when player exits outer range");
+                    ghostState = GhostState.idle;
+                    currentSpeed = slowSpeed;
+
+                    target = this.transform.position;
+                }
+                else if (distanceToPlayer <= innerRange) //player enters inner range
+                {
+                    ghostState = GhostState.hostile; //curious to hostile upon entering inner range
+                    target = player.transform.position;
+                    currentSpeed = fastSpeed;
+                    
+                    break;
+                }
+                break;
+            case GhostState.hostile:
+                if (distanceToPlayer >= outerRange) //player exits outer range 
+                {
+                    ghostState = GhostState.wary; //hostile to wary upon exiting outer range
+                    lastSeenPlayerPos = player.transform;
+                    currentSpeed = slowSpeed;
+
+                    target = lastSeenPlayerPos.position;
+                    break;
+                }
+                break;
+            case GhostState.wary: //in wary state (state 3)
+                if (distanceToPlayer <= outerRange) //entering outer range
+                {
+                    ghostState = GhostState.hostile; //wary to hostile upon entering outer range 
+
+                    currentSpeed = fastSpeed;   
+                    target = player.transform.position;
+                    break;
+                }
+                break;
+
         }
     }
 
+    /*
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "player outer circle")
+        if(collider.gameObject.CompareTag("player")){
+            if(Vector2.Distance(player.transform.position, this.transform.position) <= innerRange) //player is in inner circle
+            {
+                switch (ghostState)
+                {
+
+                    case GhostState.idle:
+                        //state = 2;
+                        ghostState = GhostState.hostile;
+                        target = player.transform.position;
+                        //target = player.position;
+                        break;
+                    case GhostState.curious: //if curious
+                        ghostState = GhostState.hostile;
+                        target = player.transform.position;
+
+                        //set to state
+                        state = 2;
+                        //target = player.position;
+                        break;
+                    case GhostState.wary: //if wary
+                        ghostState = GhostState.hostile;
+                            //other version of this statement sets it to curious
+
+                        //set to hostile
+                        state = 2;
+                        //target = player.position;
+                        break;
+
+                            
+                    //case (GhostState.wary):
+                    //    ghostState = GhostState.curious;
+                    //    target = player.transform.position;
+                    //    break;
+                    //    case (ghostState.)
+
+                }
+            }
+        
+        }
+        //if (collider.gameObject.CompareTag("player outer circle"))
+        //if(collider.gameObject.transform.position )
+        if(true) //originally this was going to use vec2.distance() to probably check player pos, swapping to if(true) to avoid compile errors
         {
             switch (state)
             {
@@ -64,7 +209,7 @@ public class Ghost : MonoBehaviour
                     break;
             }
         }
-        else if (collider.gameObject.tag == "player inner circle")
+        else if (collider.gameObject.CompareTag("player inner circle"))
         {
             switch (state)
             {
@@ -86,7 +231,7 @@ public class Ghost : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "player outer circle")
+        if (collider.gameObject.CompareTag("player outer circle"))
         {
             switch (state)
             {
@@ -101,6 +246,7 @@ public class Ghost : MonoBehaviour
             }
         }
     }
+    */
 
     public void SetLampLit()
     {
